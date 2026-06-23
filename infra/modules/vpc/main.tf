58f67a82d3dc8c82c56891bfddbedd9342086616
Mustapha Haadi <mustaphahaadi04@gmail.com>
2026-06-23 17:13:15 +0000

    feat: add VPC and public subnet resources to Terraform configuration

---
+++ infra/modules/vpc/main.tf
@@ -0,0 +1,24 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name        = "${var.environment}-vpc"
+    Environment = var.environment
+  }
+}
+
+# Public subnets
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnet_cidr[0])
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr[count.index]
+  availability_zone       = var.azs[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name        = "${var.environment}-public-subnet-${count.index + 1}"
+    Environment = var.environment
+  }
+}
