0fe9ca0354cc91e5b71fc76b73fc126c7f6caf4a
Mustapha Haadi <mustaphahaadi04@gmail.com>
2026-06-23 18:15:42 +0000

    feat: update VPC and subnet configurations in Terraform files

--- infra/modules/vpc/variables.tf
+++ infra/modules/vpc/variables.tf
@@ -4,7 +4,7 @@ variable "vpc_cidr" {
   default     = "10.0.0.0/16"
 }
 
-variable "public_subnet_cidr" {
+variable "public_subnet_cidrs" {
   description = "The CIDR block for the public subnet"
   type        = list(string)
   default     = ["10.0.1.0/24", "10.0.2.0/24"]
@@ -16,7 +16,7 @@ variable "azs" {
   default     = ["us-east-1a", "us-east-1b"]
 }
 
-variable "private_subnet_cidr" {
+variable "private_subnet_cidrs" {
   description = "The CIDR block for the private subnet"
   type        = list(string)
   default     = ["10.0.3.0/24", "10.0.4.0/24"]
